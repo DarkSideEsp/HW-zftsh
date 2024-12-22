6e94f616f5623c58523029b8d44f61013ec7a7fd
Iaroslav Pashin <iaroslav.pash@yandex.ru>
2024-12-22 22:03:20 +0300

    Examples for task 1 added

--- task1/main.cpp
+++ task1/main.cpp
@@ -17,4 +17,11 @@ int mySqrt(int x){
 
 int main(){
     cout << mySqrt(3) << "\n";
+    cout << mySqrt(4) << "\n";
+    cout << mySqrt(5) << "\n";
+    cout << mySqrt(6) << "\n";
+    cout << mySqrt(7) << "\n";
+    cout << mySqrt(8) << "\n";
+    cout << mySqrt(9) << "\n";
+    cout << mySqrt(10) << "\n";
 }
