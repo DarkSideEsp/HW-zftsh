3abbda68f3170664142afb200bccf9470756f155
Iaroslav Pashin <iaroslav.pash@yandex.ru>
2024-12-22 22:00:51 +0300

    Task 1 solved

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 
 using namespace std;
+
+int mySqrt(int x){
+    int l = 1;
+    int r = x;
+    int mid = (l + r) / 2;
+    while(r - l > 1){
+        mid = (l + r) / 2;
+        if(mid * mid <= x) l = mid;
+        else r = mid;
+    }
+
+    return l;
+}
+
+int main(){
+    cout << mySqrt(3) << "\n";
+}
